136705bf0d5f91d9db07095d3152b22dae2d93ac
Francesco Oliva <olibartfast@gmail.com>
2025-06-14 03:40:24 +0200

    Remove convolution implementation and related files

--- convolution/reference_calc.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void referenceCalculation(const uchar4* const rgbaImage, uchar4 *const outputImage,
-                          size_t numRows, size_t numCols,
-                          const float* const filter, const int filterWidth);
